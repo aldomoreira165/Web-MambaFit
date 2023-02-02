e8e667466847fca1e4366c8b32564c5f5b000b45
Aldo Vasquez <aldovasquez2014@gmail.com>
2023-02-02 10:42:01 -0600

    empezando mejora de visualizacion de productos en carrito

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -15,13 +15,18 @@ if (productosEnCarrito) {
         const div = document.createElement('div');
         div.classList.add('carrito-producto');
         div.innerHTML = `
-        <div id="tarjeta-producto" class="card">
-            <img id="imagen-producto" src="${producto.imagen}" class="card-img-top">
-            <div id="cuerpo-tarjeta" class="card-body">
+        <div id="tarjeta-producto-carrito" class="card">
+            <div id="cuerpo-imagen-producto" class="card-body">
+                <img id="imagen-producto-carrito" src="${producto.imagen}" class="card-img-top">
+            </div>
+            <div id="cuerpo-tarjeta-carrito" class="card-body">
                 <h5 class="card-title">${producto.nombre}</h5>
                 <p id="texto-producto" class="card-text">Cantidad: ${producto.cantidad}</p>
                 <p id="texto-producto" class="card-text">Precio: Q${producto.precio}</p>
                 <p id="texto-producto" class="card-text">Subtotal: Q${(producto.precio * producto.cantidad).toFixed(2)}</p>
+            </div>
+            <div id="cuerpo-eliminar-producto" class="card-body">
+                <a href=""><i class="fas fa-times"></i></a>
             </div>
         </div>
         `;
