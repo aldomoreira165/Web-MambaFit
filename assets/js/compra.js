3cc813873cbc8d0c5dc561e6571381aeabe217fe
Aldo Vasquez <aldovasquez2014@gmail.com>
2023-02-07 09:40:52 -0600

    envio de correos electronicos

--- assets/js/compra.js
+++ assets/js/compra.js
@@ -1,15 +1,19 @@
-
 const detalleCompra = document.querySelector('#detalleInput');
 var listaProductos = JSON.parse(localStorage.getItem('productos-en-carrito'));
-var textoCompra;
+var textoCompra = '';
+var totalCompra = 0;
 
 listaProductos.forEach(producto => {
     textoCompra += `
     Producto: ${producto.nombre}
-    Precio: ${producto.precio} 
     Cantidad: ${producto.cantidad}
+    Precio: ${producto.precio} 
+    Subtotal: Q${(producto.precio * producto.cantidad).toFixed(2)}
    `
+   totalCompra += producto.precio * producto.cantidad
 });
+
+textoCompra += `\n    Total: Q${totalCompra.toFixed(2)}`;
 
 detalleCompra.value = textoCompra;
 
